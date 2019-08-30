a025785f9d5ad5ff391e933683b8e92b6306aac3
福浩 杨 <Fuhao.yang@outlook.com>
2019-08-31 00:22:30 +0800

    Create Server.sql
    The basic database based on PostgreSQL.

---
+++ Server.sql
@@ -0,0 +1,64 @@
+create table UserInfo(
+    id integer primary key,
+    username text,
+    password text,
+    createTime timestamp,   --日期和时间(无时区)
+    alive boolean,
+    ip cidr,                --ip地址
+    avatar bit varying,     --不限长比特流
+    birthday timestamp,
+    signature text,
+    isMale boolean,
+    nickname text
+);
+
+create table Friend(
+    owner integer references UserInfo(id),
+    friend integer references UserInfo(id),
+    groupName text,
+    mute boolean,
+    primary key (owner, friend)
+);
+
+create table ChatGroup(
+    id integer primary key ,
+    name text
+);
+
+create table User_in_Group(
+    userId integer references UserInfo(id),
+    groupId integer references ChatGroup(id),
+    mute boolean,
+    primary key (userId, groupId)
+);
+
+create type MsgType as enum ('text', 'file', 'image');
+
+create table Message(
+    id integer primary key,
+    sender integer references UserInfo(id),
+    nicknameOfSender text references UserInfo(nickname),
+    receiver integer references UserInfo(id),
+    type MsgType,
+    createTime timestamp,
+    editedTime timestamp,
+    isToGroup boolean
+);
+
+create table Message_text(
+    tid integer primary key references Message(id),
+    mid integer,
+    content text
+);
+
+create table Message_file(
+    tid integer primary key references Message(id),
+    mid integer,
+    content text
+);
+
+create table Message_image(
+    tid integer primary key references Message(id),
+    mid integer,
+    content text
+);
