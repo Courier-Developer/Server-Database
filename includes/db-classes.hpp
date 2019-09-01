97eefcc1b4e61437f8f56503977805024b60d96a
FKYnJYQ <loveress01@gmail.com>
2019-09-01 16:16:27 +0800

    add more functions and class definition.

--- includes/db-classes.hpp
+++ includes/db-classes.hpp
@@ -22,3 +22,33 @@ struct UserInfo{
     bool isMale;
     std::string nickname;
 };
+
+/// 好友和群聊好友都用这个？
+struct Friend{
+    int uid;
+    std::string group; ///< 哪个用户组，是好友与好友关系
+    std::string username;
+    std::string createdTime;
+    std::string lastLoginTime;
+    // TODO avatar
+    std::string birthday;
+    bool alive;
+    bool isMale;
+    std::string ip;
+    std::string nickname;
+};
+
+struct ChatGroup{
+    int id;
+    std::string name;
+};
+
+class Message{
+    int id;
+    int sender;
+    int reciver;
+    std::string type;
+    std::string createdTime;
+    std::string editedTime;
+    bool isToGroup;
+};
